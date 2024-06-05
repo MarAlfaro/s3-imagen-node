f355b1e039aef3b1dd0fb92a2e6651c245f82c4e
Yancy Alfaro <alfaromargarita556@gmail.com>
2024-06-05 17:37:12 -0600

    Duración de 60s imagen

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,20 @@ import MoonIcon from './images/oscuro.png';
 import SunIcon from './images/claro.png';
 
 function App() {
+  // Estados del componente
   const [image, setImage] = useState("");
   const [selectedImage, setSelectedImage] = useState(null);
   const [darkMode, setDarkMode] = useState(false);
+  const [showImage, setShowImage] = useState(false);
 
+  // Efecto para cargar el modo oscuro desde el almacenamiento local
   useEffect(() => {
     const darkModePreference = localStorage.getItem('darkMode') === 'true';
     setDarkMode(darkModePreference);
     document.documentElement.setAttribute('data-theme', darkModePreference ? 'dark' : 'light');
   }, []);
 
+  // Función para alternar entre el modo claro y oscuro
   const toggleDarkMode = () => {
     const newMode = !darkMode;
     setDarkMode(newMode);
@@ -22,6 +26,7 @@ function App() {
     document.documentElement.setAttribute('data-theme', newMode ? 'dark' : 'light');
   };
 
+  // Función para manejar la subida de la imagen
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -35,6 +40,12 @@ function App() {
         const { data } = await axios.post("http://localhost:3001/api/s3", formData, { headers });
         if (data.success) {
           setImage(data.data.url);
+          setShowImage(true);
+
+          // Oculta la imagen después de 60 segundos
+          setTimeout(() => {
+            setShowImage(false);
+          }, 60000); 
         }
       }
     } catch (error) {
@@ -42,18 +53,20 @@ function App() {
     }
   };
 
+  // Función para manejar el cambio de la imagen seleccionada
   const handleFileChange = (ev) => {
     const file = ev.target.files && ev.target.files[0];
     if (file) setSelectedImage(file);
   };
 
+  // Renderiza el componente
   return (
     <div className="container">
       <div className="theme-toggle" onClick={toggleDarkMode}>
         {darkMode ? <img src={SunIcon} alt="Modo Claro" /> : <img src={MoonIcon} alt="Modo Oscuro" />}
       </div>
       <h1>Subir imagen a S3</h1>
-      {image && <img alt="Uploaded" src={image} className="image" />}
+      {showImage && image && <img alt="Uploaded" src={image} className="image" />}
       <form onSubmit={onSubmit} className="form">
         <label htmlFor="image">Selecciona tu imagen</label>
         <input id="image" type="file" onChange={handleFileChange} />
@@ -64,4 +77,3 @@ function App() {
 }
 
 export default App;
-
